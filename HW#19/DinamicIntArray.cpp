516d80e89ca763b161d2320c754ce01b92ef5621
ChesterKing9 <chesterking2209@gmail.com>
2025-03-10 11:52:29 -0400

    HW#19 issuse resolved

--- HW#19/DinamicIntArray.cpp
+++ HW#19/DinamicIntArray.cpp
@@ -3,26 +3,21 @@
 #include <stdexcept>
 #include <iostream>
 
-// Default constructor
 DynamicIntArray::DynamicIntArray() : m_data(nullptr), m_size(0) {}
 
-// Constructor with size
 DynamicIntArray::DynamicIntArray(std::size_t size) : m_size(size) {
     m_data = new int[m_size] {};
 }
 
-// Destructor
 DynamicIntArray::~DynamicIntArray() {
     delete[] m_data;
 }
 
-// Copy constructor
 DynamicIntArray::DynamicIntArray(const DynamicIntArray& other) : m_size(other.m_size) {
     m_data = new int[m_size];
     std::copy(other.m_data, other.m_data + m_size, m_data);
 }
 
-// Assignment operator
 DynamicIntArray& DynamicIntArray::operator=(const DynamicIntArray& other) {
     if (this != &other) {
         delete[] m_data;
@@ -33,7 +28,6 @@ DynamicIntArray& DynamicIntArray::operator=(const DynamicIntArray& other) {
     return *this;
 }
 
-// Operator []
 int& DynamicIntArray::operator[](std::size_t index) {
     if (index >= m_size) {
         throw std::out_of_range("Index out of range");
@@ -41,32 +35,38 @@ int& DynamicIntArray::operator[](std::size_t index) {
     return m_data[index];
 }
 
-// Clear method
 void DynamicIntArray::clear() {
     delete[] m_data;
     m_data = nullptr;
     m_size = 0;
 }
 
-// Comparison operators
 bool DynamicIntArray::operator==(const DynamicIntArray& other) {
-    if (m_size != other.m_size) return false;
-    for (std::size_t i = 0; i < m_size; ++i) {
-        if (m_data[i] != other.m_data[i]) return false;
+    if (this == &other) {
+        return true;
     }
-    return true;
+
+    if (m_size != other.m_size) {
+        return false;
+    }
+
+    for (std::size_t i = 0; i < m_size; ++i) {
+        if (m_data[i] != other.m_data[i]) {
+            return false;
+        }
+    }
+
+    return true; 
 }
 
 bool DynamicIntArray::operator!=(const DynamicIntArray& other) {
     return !(*this == other);
 }
 
-// Get size method
 std::size_t DynamicIntArray::getSize() const {
     return m_size;
 }
 
-// Set size method
 void DynamicIntArray::setSize(std::size_t newSize) {
     int* newData = new int[newSize] {};
     std::size_t minSize = (newSize < m_size) ? newSize : m_size;
@@ -76,7 +76,6 @@ void DynamicIntArray::setSize(std::size_t newSize) {
     m_size = newSize;
 }
 
-// Push back method
 void DynamicIntArray::push_back(int element) {
     setSize(m_size + 1);
     m_data[m_size - 1] = element;
